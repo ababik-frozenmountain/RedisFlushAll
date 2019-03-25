e79564863db2cb03db2f04e5b2f6bd7aea960f02
Alexey Babik <>
2019-03-25 13:44:07 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisFlushAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "127.0.0.1:52009,allowAdmin=true";
+            var redis = ConnectionMultiplexer.Connect(connectionString);
+
+            foreach (var endpoint in redis.GetEndPoints())
+            {
+                var server = redis.GetServer(endpoint);
+                server.FlushDatabase();
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
